42febb4cb7e27a566695bcd74f58e6ebfcc076e4
Zakariyaa Karhouni <zakariyakarhouni46@gmail.com>
2026-08-20 15:37:18 +0100

    Preserve music playback across page navigations

--- src/components/music/global-music-player-bar.tsx
+++ src/components/music/global-music-player-bar.tsx
@@ -125,7 +125,7 @@ export function GlobalMusicPlayerBar() {
   }, [])
 
   React.useEffect(() => () => {
-    stopAll()
+    if (progressInterval.current) clearInterval(progressInterval.current)
     if (sleepTimerRef.current) clearTimeout(sleepTimerRef.current)
   }, [])
 
